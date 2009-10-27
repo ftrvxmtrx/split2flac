7fcbd3ac63c32295478865e64391dbe5e62a26c4
Serge Zirukin <ftrvxmtrx@gmail.com>
2009-10-27 12:07:13 +0200

    typo fixed

--- split2flac.sh
+++ split2flac.sh
@@ -218,7 +218,7 @@ then
 fi
 
 GETTAG="cueprint -n 1 -t"
-VALIDATE="sed s/[^[:space:][:alnum:]]&\-_#,.'\"//g"
+VALIDATE="sed s/[^[:space:][:alnum:]&\-_#,.'\"]//g"
 
 # get common tags
 TAG_ARTIST=$(${GETTAG} %P "${CUE}")
